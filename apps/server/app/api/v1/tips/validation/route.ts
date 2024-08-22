cdef3157f02fca2e5f08e1687a03078c66483fea
cryptods8 <dusoo.s+ds8@gmail.com>
2024-08-22 13:15:29 +0200

    caching neynar

--- apps/server/app/api/v1/tips/validation/route.ts
+++ apps/server/app/api/v1/tips/validation/route.ts
@@ -1,67 +1,8 @@
+import { cache } from "@/app/api/cache";
 import { NextRequest, NextResponse } from "next/server";
-import { Redis } from "@upstash/redis";
-
-function redis() {
-  if (!process.env.REDIS_API_URL || !process.env.REDIS_API_TOKEN) {
-    return null;
-  }
-  const redis = new Redis({
-    url: process.env.REDIS_API_URL,
-    token: process.env.REDIS_API_TOKEN,
-  });
-  return redis;
-}
-
-const cache = {
-  get: async <V>(key: string): Promise<V | null> => {
-    const r = redis();
-    if (!r) {
-      return Promise.resolve(null);
-    }
-    const value = await r.get(key);
-    return value as V | null;
-  },
-  set: async <V>(
-    key: string,
-    value: V,
-    customExpiry?: number
-  ): Promise<void> => {
-    const r = redis();
-    if (!r) {
-      return;
-    }
-    // 7-day expiry
-    r.setex(key, customExpiry ?? 7 * 24 * 60 * 60, value);
-  },
-};
+import { fetchCastFromNeynar } from "./fetch-cast-from-neynar";
 
 export const dynamic = "force-dynamic";
-
-interface Cast {
-  hash: string;
-  text: string;
-}
-interface CastResponse {
-  cast?: Cast;
-}
-
-async function fetchCast(castUrl: string) {
-  if (!process.env.NEYNAR_API_KEY) {
-    throw new Error("NEYNAR_API_KEY not set");
-  }
-  const url = `https://api.neynar.com/v2/farcaster/cast?identifier=${castUrl}&type=url`;
-  const options = {
-    method: "GET",
-    headers: {
-      accept: "application/json",
-      api_key: process.env.NEYNAR_API_KEY,
-    },
-  };
-  const castResp = await fetch(url, options);
-  const cast = await castResp.json();
-
-  return cast as CastResponse;
-}
 
 interface TipResponse {
   username?: string;
@@ -78,20 +19,6 @@ async function fetchTip(hash: string) {
 }
 
 async function fetchTipFromDegenTips(hash: string) {
-  /*
-  {
-    "snapshot_day": "2024-08-19T00:00:00.000Z",
-    "timestamp": "2024-08-19T09:43:26.000Z",
-    "cast_hash": "\\x8807fff56ceb6a285ed5c1e092828dd189361cee",
-    "fid": "11124",
-    "tip_status": "valid",
-    "tip_type": "regular",
-    "tip_amount": "200",
-    "rolling_daily_tip_amount": "400",
-    "tip_allowance": "9462"
-  }
-  */
-
   const tipResp = await fetch(
     `https://api.degen.tips/airdrop2/tips?hash=\\${hash.substring(1)}`
   );
@@ -143,7 +70,7 @@ export async function GET(req: NextRequest) {
       }
     }
 
-    const { cast } = await fetchCast(castUrl);
+    const cast = await fetchCastFromNeynar(castUrl);
     if (!cast) {
       await cache.set<CachedTipValidationResult>(
         cacheKey,
